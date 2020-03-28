98b3967df2a4fb6570b9e90cf0a4f8f631b536b3
A-Thousand-Suns <787545023@qq.com>
2020-03-28 21:49:44 +0800

    commit

--- 中文分词/wordSeg.py
+++ 中文分词/wordSeg.py
@@ -1,20 +1,28 @@
 #coding = utf-8
+import re
 class wordSehmentation:
     def __init__(self, filePath, dicPath):
         self.filePath = filePath
         self.dicPath = dicPath
 
     def readFile(self):
-        str = ''
+        content = ''
         with open(self.filePath, 'r', encoding='utf-8') as file:
             for i in file.readlines():
-                str = str + i
-        print()
+                content = content + i
+        content.replace('\n', '')
+        content.replace('\r', '')
+        #print(content)
+        return content
 
-
+    def cutSentence(self):
+        content = self.readFile()
+        pattern = r'：|？|,|\.|/|;|\'|`|\[|\]|<|>|\?|:|"|\{|\}|\~|!|@|#|\$|%|\^|&|\(|\)|-|=|\_|\+|，|。|、|；|‘|’|【|】|·|！| |…|（|）'
+        for i in re.split(pattern, content):
+            print(i)
 
 if __name__ == '__main__':
     a = 'testCase.txt'
     b = 0
     c = wordSehmentation(a, b)
-    c.readFile()
+    c.cutSentence()
